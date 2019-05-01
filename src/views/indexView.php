9d1898dd28ce1166ae7f98f3bd89fcc1233dba33
Rageeb Mahtab <rmahtab0079@bths.edu>
2019-05-01 17:13:38 -0400

    Edited index.php

---
+++ src/views/indexView.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Amazon Smile Homepage</title>
+  </head>
+  <body>
+    <h1>Amazon</h1>
+  </body>
+</html>
