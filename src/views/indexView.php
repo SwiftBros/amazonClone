9b007efaa0369a206d98c50bc186f40e7c664c35
Matthew <matthew.b.rodriguez@gmail.com>
2019-05-01 17:20:52 -0400

    Test indexView commit

--- src/views/indexView.php
+++ src/views/indexView.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Amazon Smile Homepage</title>
-  </head>
-  <body>
-    <h1>Amazon</h1>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Amazon Smile Homepage</title>
+    </head>
+    <body>
+        <h1>Amazon</h1>
+        <h2>Test</h2>
+    </body>
 </html>
