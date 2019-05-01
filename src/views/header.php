9a652dfe24626976d286cf8c003f4906049e873b
Matthew <matthew.b.rodriguez@gmail.com>
2019-05-01 17:37:06 -0400

    Edit header.php

--- src/views/header.php
+++ src/views/header.php
@@ -1,10 +1,18 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-        <meta charset="utf-8">
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap stylesheet -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <!-- Note: External stylesheets must come after Bootstrap stylesheet -->
+        <!-- <link href="weather-styles.css" rel="stylesheet" type="text/css" /> -->
         <title>Header</title>
     </head>
     <body>
-        
+
     </body>
 </html>
