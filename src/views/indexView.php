88f978a43565bc6a2a4b0ce94c3ecc31d37f908c
Matthew <matthew.b.rodriguez@gmail.com>
2019-05-01 17:35:15 -0400

    Edit indexView.php

--- src/views/indexView.php
+++ src/views/indexView.php
@@ -1,14 +1,21 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-        <meta charset="utf-8">
-        <title>Amazon Smile Homepage</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap stylesheet -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <!-- Note: External stylesheets must come after Bootstrap stylesheet -->
+        <!-- <link href="weather-styles.css" rel="stylesheet" type="text/css" /> -->
+
+        <title>AmazonClone: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more</title>
     </head>
+
     <body>
-
-        <h1>Amazon</h1>
-        <h2>Test</h2>
-
-        <h3> dfjlasd </h3>
+        <div>
+        </div>
     </body>
 </html>
