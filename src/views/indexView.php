cd9853ad914acdea3c985fae4d45ccbacc48697f
Matthew <matthew.b.rodriguez@gmail.com>
2019-05-02 19:58:17 -0400

    Style homele homepage

--- src/views/indexView.php
+++ src/views/indexView.php
@@ -9,14 +9,52 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <!-- Note: External stylesheets must come after Bootstrap stylesheet -->
-        <!-- <link href="weather-styles.css" rel="stylesheet" type="text/css" /> -->
+        <link href="indexView-styles.css" rel="stylesheet" type="text/css" />
 
         <title>AmazonClone: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more</title>
     </head>
 
     <body>
-        <h1>Test</h1>
-        <div>
+        <div class="container">
+            <div class="row justify-content-around home-cards">
+                <div class="col-md-3 col-sm-3 col-xs-12 card">
+                    <div class="row">
+                        <div class="col-4">
+                            <img src="images/profile.png" alt="profile_pic" class="img-fluid profile"/>
+                        </div>
+                        <div class="col-8 pl-0">
+                            <span class="card-title">Hi, Matthew</span>
+                            <div class="card-text"><span style="color:#C35629">AmazonSmile</span> <span>donations</span></div>
+                            <div class="card-text">generated: $0.00</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-3 col-xs-12 card">
+                    <label class="card-title">Watch the final season</label>
+                    <img src="images/tyrion-fake.jpg" alt="Game of Thrones image" class="img-fluid" />
+                    <div class="card-text">New episodes available every Sunday on HBO with Prime Video Channels</div>
+
+                    <div class="card-text" style="padding-top: 20px; color:#0645AD">Watch with a subscription</div>
+                </div>
+
+                <div class="col-md-3 col-sm-3 col-xs-12 card">
+                    <label class="card-title">Prime member deals</label>
+                    <img src="images/member-deals.jpg" alt="Prime member deals image" class="img-fluid" />
+                    <div class="card-text">Deals and products just for Prime members</div>
+
+                    <div class="card-text" style="padding-top: 20px; color:#0645AD">Learn more</div>
+                </div>
+
+                <div class="col-md-3 col-sm-3 col-xs-12 card">
+                    <label class="card-title">Deal of the day</label>
+                    <img src="images/headphones-01.jpeg" alt="Daily deal" class="img-fluid" />
+                    <div class="card-text">Save up to 25% on Headphones</div>
+                    <div class="card-text"><span class="daily-price">$25</span><div>
+                    <div class="card-text" style="padding-top: 20px; color:#0645AD">See all deals</div>
+                </div>
+            </div>
         </div>
+
     </body>
 </html>
