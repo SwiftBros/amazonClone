46eac9b14b5d7a2dc8a12d5cbb441b63f084f891
Matthew <matthew.b.rodriguez@gmail.com>
2019-05-03 16:29:40 -0400

    Working on login view

--- src/views/indexView.php
+++ src/views/indexView.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
         <!-- Note: External stylesheets must come after Bootstrap stylesheet -->
-        <link href="indexView-styles.css" rel="stylesheet" type="text/css" />
+        <link href="CSS/indexView-styles.css" rel="stylesheet" type="text/css" />
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
---
+++ src/views/loginView.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap stylesheet -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <!-- Note: External stylesheets must come after Bootstrap stylesheet -->
+        <link href="CSS/loginView-styles.css" rel="stylesheet" type="text/css" />
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+        <title>Amazon Sign In</title>
+    </head>
+
+    <body>
+        <p class="aligncenter">
+            <img src="images/amazon-smile.png" alt="Amazon Smile Logo" class="img-fluid logo" />
+        </p>
+        <div class="container">
+            <div class="row justify-content-center login-row">
+                <div class="col-md-5 col-sm-5 col-xs-12 login-box">
+                    <div class="login-text">
+                        <span class="shop">You shop.</span>
+                        <span class="gives">Amazon gives.</span>
+
+                        <div><br />Amazon donates 0.5% of the price of your eligible AmazonSmile purchases to the charitable organization of your choice.</div>
+                        <br />
+                        <div>AmazonSmile is the same Amazon you know. Same products, same prices, same service.</div>
+                        <br />
+                        <div>Support your charitable organization by starting your shopping at smile.amazon.com</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
