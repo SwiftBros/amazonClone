5c8382454aec2b5f2b1fbee59b0c9c29fd3c4af0
Rageeb Mahtab <rmahtab0079@bths.edu>
2019-05-03 20:26:55 -0400

    Header File

--- src/views/header.php
+++ src/views/header.php
@@ -8,11 +8,43 @@
         <!-- Bootstrap stylesheet -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
+
         <!-- Note: External stylesheets must come after Bootstrap stylesheet -->
-        <!-- <link href="weather-styles.css" rel="stylesheet" type="text/css" /> -->
+        <link href="main.css" rel="stylesheet" type="text/css" />
         <title>Header</title>
     </head>
     <body>
 
-    </body>
+      <div id='navBar'>
+      <div id='topHalf'>
+        <div id='logoWrapper'>
+        <!-- <img id='logo' src="http://www.userlogos.org/files/logos/ArkAngel06/Amazon.png" /> -->
+        <img id='logo' alt="AmazonLogo" src="images/amaz.png" />
+        </div>
+        <input id="searchBar" type="text"/>
+        <img id='backToSchool' src="http://blog.neurogistics.com/wp-content/uploads/2014/08/Back-To-School-Special-Banner.jpg" alt="" />
+
+      </div>
+
+
+      <div id='bottomHalf'>
+        <div id='department'>Departments</div>
+        <div id='sections'>
+          <div class="section">Your Amazon.com</div>
+          <div class="section">Today's Deals</div>
+          <div class="section">Gift Cards & Registry</div>
+          <div class="section">Sell</div>
+          <div class="section">Help</div>
+        </div>
+        <div id='accountStuff'>
+          <div class="section">Your Account</div>
+          <div class="section">Try Prime</div>
+          <div class="section">Lists</div>
+          <div class="section">Cart</div>
+        </div>
+      </div>
+    </div>
+
+
+</body>
 </html>
