4d67e1c399a88c3e89006ae75c9bbc284e690887
Rageeb Mahtab <rmahtab0079@bths.edu>
2019-05-04 18:54:07 -0400

    New Changes

--- src/views/footer.php
+++ src/views/footer.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
@@ -6,8 +6,8 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="CSS/header.css">
     </head>
-    <body>
-        <nav id="footer" class="navbar fixed-bottom navbar-expand-sm">
+    <body> -->
+        <nav id="footer" class="navbar bottom navbar-expand-sm">
             <ul class="navbar-nav">
               <li class="nav-item">
                 <a id="firstli" class="nav-link" href="#">Get to Know Us</a>
--- src/views/indexView.php
+++ src/views/indexView.php
@@ -1,18 +1,13 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-        <!-- Required meta tags -->
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap stylesheet -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-        <!-- Note: External stylesheets must come after Bootstrap stylesheet -->
         <link href="CSS/indexView-styles.css" rel="stylesheet" type="text/css" />
 
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -20,8 +15,13 @@
         <title>AmazonClone: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more</title>
     </head>
 
-    <body>
+    <body> -->
         <!-- Carousel slider -->
+
+
+<?php include 'header.php'; ?>
+
+ <!-- Carousel slider -->
         <div class="carousel-slideshow">
             <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                 <div class="carousel-inner">
@@ -101,6 +101,13 @@
                 </div>
             </div>
         </div>
+    </div>
+</div>
+<br>
+<br>
+<br>
 
-    </body>
-</html>
+<?php include 'footer.php'; ?>
+
+    <!-- </body>
+</html> -->
