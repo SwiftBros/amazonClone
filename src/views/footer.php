c1a96257efdfef03f2cad6d5a6a264f5ff619ad9
Rageeb Mahtab <rmahtab0079@bths.edu>
2019-05-04 18:32:46 -0400

    Header and Footer done

--- src/views/footer.php
+++ src/views/footer.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title></title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <link rel="stylesheet" href="header.css">
+        <link rel="stylesheet" href="CSS/header.css">
     </head>
     <body>
         <nav id="footer" class="navbar fixed-bottom navbar-expand-sm">
