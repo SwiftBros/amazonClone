3b4650699b2138629b715725ce7337c652b85e21
Rageeb Mahtab <rmahtab0079@bths.edu>
2019-05-06 18:41:55 -0400

    Backend

--- src/views/header.php
+++ src/views/header.php
@@ -1,3 +1,41 @@
+<?php # Script 12.4 - loggedin.php
+// The user is redirected here from login.php.
+
+session_start();
+// If no cookie is present, redirect the user:
+if (!isset($_SESSION['user_id'])) {
+
+	// Need the functions:
+	require ('functions.php');
+	redirect_user();
+
+}
+
+// Set the page title and include the HTML header:
+
+$pageTitle = "Welcome";
+
+include('mysqli_connect.php');
+
+$query = "SELECT user_id, name, email FROM users WHERE user_id = ".$_SESSION['user_id'];
+$run = mysqli_query($dbc, $query);
+
+if (mysqli_num_rows($run) == 1){
+	$row = mysqli_fetch_array($run, MYSQLI_ASSOC);
+	// Print a customized message:
+	// echo "<h1>Welcome!</h1>
+	// <br />&nbsp;
+	// <p>You are now logged in, ".$row['name']."!</p>
+	// <p style='text-align:center;'><img src='https://via.placeholder.com/150x150'><br>
+	// <strong>".$row['name']."</strong><br>
+	// ".$row['email']."</p>
+	// <p style='text-align:center'><a href=\"logout.php\">Logout</a></p>";
+}else{
+	echo "Error!";
+}
+mysqli_close($dbc);
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -57,7 +95,7 @@
               <a class="nav-link" href="#">Today's Deals</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Dewan's Amazon.com</a>
+              <a class="nav-link" href="#"><?php echo $row['name']; ?>'s Amazon.com</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Buy Again</a>
@@ -66,7 +104,10 @@
               <a class="nav-link" href="#">Help</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Hello, Dewan Accounts & Lists</a>
+               <?php
+                    echo "<a class=\"nav-link\" href=\"#\">Hello, ".$row['name']. "Accounts & Lists</a>";
+               ?>
+              <!-- <a class="nav-link" href="#">Hello, Dewan Accounts & Lists</a> -->
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Orders</a>
