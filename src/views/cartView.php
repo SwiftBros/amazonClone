a2288687515d53efe0821abd733769e5076fe26d
Matthew <matthew.b.rodriguez@gmail.com>
2019-05-07 16:55:13 -0400

    working on ccart view

--- src/views/cartView.php
+++ src/views/cartView.php
@@ -1,1 +1,15 @@
-<?php  ?>
+<?php include 'header.php'; ?>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-9">
+                Shopping Cart
+            </div>
+
+            <div class="col-3 checkout-card">
+                <div id="cart-label">Subtotal</div>
+            </div>
+        </div>
+    </div>
+
+<?php include 'footer.php'; ?>
