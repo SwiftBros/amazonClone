e968e17ccd60b075b0bdb2ea31df8aec60614448
Matthew <matthew.b.rodriguez@gmail.com>
2019-05-08 18:29:09 -0400

    INSERT DONE

--- src/views/cartView.php
+++ src/views/cartView.php
@@ -44,54 +44,56 @@
     $count = mysqli_num_rows($run);
 
     if($count > 0) {
-        
-    }
-?>
+        while ($row = mysqli_fetch_array($run, MYSQLI_ASSOC)) {
+            echo "
+            <!-- RECYCLE STARTING POINT -->
+            <div class='row cart-table'>
+                <div class='col-8'> <!-- column for item details-->
+                    <div class='row item-row'>
+                        <div class='col-3 item-image'>
+                            <img src='" .$row['cart_product_img_url']. "' />
+                            <br />
+                        </div>
 
-                <!-- RECYCLE STARTING POINT -->
-                <div class="row cart-table">
-                    <div class="col-8"> <!-- column for item details-->
-                        <div class="row item-row">
-                            <div class="col-3 item-image">
-                                <img src="images/headphones-2.jpg" />
-                                <br />
-                            </div>
+                        <div class='col-9'>
+                            <span class='item-desc'>" .$row['product_name']. "</span><br />
 
-                            <div class="col-9">
-                                <span class="item-desc">On Ear Headphones with Mic, Vogek Lightweight Portable Fold-Flat Stereo Bass Headphones with 1.5M Tangle Free Cord and Microphone-Black</span><br />
+                            <!-- TODO: Add this if item is prime -->
+                            <img src='images/prime.png' class='prime' />
+                            <br />
 
-                                <!-- TODO: Add this if item is prime -->
-                                <img src="images/prime.png" class="prime" />
-                                <br />
+                            <input type='checkbox' /><span id='checkbox-gift'>&nbsp;&nbsp;This is a gift</span>
+                            <span class='hyperlink'> Learn more</span>
+                            <br />
 
-                                <input type="checkbox" /><span id="checkbox-gift">&nbsp;&nbsp;This is a gift</span>
-                                <span class="hyperlink"> Learn more</span>
-                                <br />
-
-                                <!-- TODO: remove this from HTML and DELETE from database on click -->
-                                <input type="submit" value="Delete" />
-                            </div>
+                            <!-- TODO: remove this from HTML and DELETE from database on click -->
+                            <input type='submit' value='Delete' />
                         </div>
                     </div>
-
-                    <div class="col-3"> <!-- Column for price -->
-                        <div id="cart-price">$12.99</div>
-                    </div>
-
-                    <div class="col-1 quantity-form"> <!-- Column for quantity drop down -->
-                        <form action="">
-                            <select id="pick-quantity">
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="-1" id="quantity-more">6+</option>
-                            </select>
-                        </form>
-                    </div>
                 </div>
-                <!-- RECYCLE ENDING POINT -->
+
+                <div class='col-3'> <!-- Column for price -->
+                    <div id='cart-price'>$" .$row['product_price_dollars']. "."  . $row['product_price_cents']. "</div>
+                </div>
+
+                <div class='col-1 quantity-form'> <!-- Column for quantity drop down -->
+                    <form action=''>
+                        <select id='pick-quantity'>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                            <option value='-1' id='quantity-more'>6+</option>
+                        </select>
+                    </form>
+                </div>
+            </div>
+            <!-- RECYCLE ENDING POINT -->
+            ";
+        }
+    }
+?>
 
             </div>
 
