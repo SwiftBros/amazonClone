acdaf0c9a32a0c87176929e564e849e4a8de2ba9
Matthew <matthew.b.rodriguez@gmail.com>
2019-05-08 00:03:23 -0400

    cart template done, just need to add js functionality

--- src/views/cartView.php
+++ src/views/cartView.php
@@ -29,14 +29,21 @@
                             <div class="col-9">
                                 <span class="item-desc">On Ear Headphones with Mic, Vogek Lightweight Portable Fold-Flat Stereo Bass Headphones with 1.5M Tangle Free Cord and Microphone-Black</span><br />
 
-                                <!-- Add this if item is prime -->
-                                <!-- TODO: Add JS/jQuery functionality for dates (+2 days from current_date)-->
+                                <!-- TODO: Add this if item is prime -->
                                 <img src="images/prime.png" class="prime" />
+                                <br />
+
+                                <input type="checkbox" /><span id="checkbox-gift">&nbsp;&nbsp;This is a gift</span>
+                                <span class="hyperlink"> Learn more</span>
+                                <br />
+
+                                <!-- TODO: remove this from HTML and DELETE from database on click -->
+                                <input type="submit" value="Delete" />
                             </div>
                         </div>
                     </div>
 
-                    <div class="col-3"> <!-- column for price -->
+                    <div class="col-3"> <!-- Column for price -->
                         <div id="cart-price">$12.99</div>
                     </div>
 
@@ -48,7 +55,52 @@
                                 <option value="3">3</option>
                                 <option value="4">4</option>
                                 <option value="5">5</option>
-                                <option value="">5+</option> <!-- TODO: js onclick replace this with an input textbox -->
+                                <option value="">6+</option> <!-- TODO: js onclick replace this with an input textbox -->
+                            </select>
+                        </form>
+                    </div>
+                </div>
+                <!-- RECYCLE ENDING POINT -->
+
+                <!-- RECYCLE STARTING POINT -->
+                <div class="row cart-table">
+                    <div class="col-8"> <!-- column for item details-->
+                        <div class="row item-row">
+                            <div class="col-3 item-image">
+                                <img src="images/headphones-2.jpg" />
+                                <br />
+                            </div>
+
+                            <div class="col-9">
+                                <span class="item-desc">On Ear Headphones with Mic, Vogek Lightweight Portable Fold-Flat Stereo Bass Headphones with 1.5M Tangle Free Cord and Microphone-Black</span><br />
+
+                                <!-- TODO: Add this if item is prime -->
+                                <img src="images/prime.png" class="prime" />
+                                <br />
+
+                                <input type="checkbox" /><span id="checkbox-gift">&nbsp;&nbsp;This is a gift</span>
+                                <span class="hyperlink"> Learn more</span>
+                                <br />
+
+                                <!-- TODO: remove this from HTML and DELETE from database on click -->
+                                <input type="submit" value="Delete" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-3"> <!-- Column for price -->
+                        <div id="cart-price">$12.99</div>
+                    </div>
+
+                    <div class="col-1 quantity-form"> <!-- Column for quantity drop down -->
+                        <form action="">
+                            <select name="pick-quantity">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="">6+</option> <!-- TODO: js onclick replace this with an input textbox -->
                             </select>
                         </form>
                     </div>
@@ -59,10 +111,14 @@
 
             <div class="col-3">
                 <div class="checkout-card">
-                    <div id="subtotal-label">Subtotal (1 item): <span style="color: #B12E25;">$12.99</span></div>
+                    <!-- TODO: Dynamically set price -->
+                    <div id="subtotal-label">Subtotal (2 items): <span style="color: #B12E25;">$12.99</span></div>
                     <input type="checkbox" /><span id="checkbox-gift">&nbsp;&nbsp;This order contains a gift</span>
                     <br /><br />
-                    <input type="submit" value="Proceed to checkout" />
+
+                    <form action="transactionSuccessful.php" method="post">
+                        <input type="submit" value="Proceed to checkout" />
+                    </form>
                 </div>
             </div>
         </div>
--- src/views/transactionSuccessful.php
+++ src/views/transactionSuccessful.php
@@ -1,1 +1,18 @@
-<?php  ?>
+<?php
+
+echo "<h1>Thank you for your purchase!</h1>"
+
+// TODO: Code to delete rows from cart table
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <p style='text-align:center'><a href="indexView.php">Continue shopping</a></p>
+    </body>
+</html>
